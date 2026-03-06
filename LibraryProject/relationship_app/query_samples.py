81fd2d35c854c4b911d65ed539439db715a6af29
endale1221-star <endalegashaw2023@gmail.com>
2026-03-06 12:09:39 +0300

    changing the root path

--- LibraryProject/relationship_app/query_samples.py
+++ LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_models.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
