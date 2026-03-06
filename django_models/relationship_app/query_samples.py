4f9ea40a514927c0094ddaddf7c5518dd6164dbd
endale1221-star <endalegashaw2023@gmail.com>
2026-03-06 11:44:37 +0300

    Implement Django model relationships using ForeignKey, ManyToMany, and OneToOne

---
+++ django_models/relationship_app/query_samples.py
@@ -0,0 +1,25 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_models.settings")
+django.setup()
+
+from relationship_app.models import Author, Book, Library, Librarian
+
+
+def get_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    return books
+
+
+def get_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    return books
+
+
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = library.librarian
+    return librarian
